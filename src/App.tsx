db7672d0faa2d175f9dc5d932dfff8c47aabda4b
nazim-belkadi <belkadinazim900@gmail.com>
2025-05-23 15:13:58 +0100

    Fix theme provider implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import Users from "./pages/Users";
 import Categories from "./pages/Categories";
@@ -14,23 +12,21 @@ import Products from "./pages/Products";
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="system" enableSystem attribute="class">
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/payments" element={<Payments />} />
-            <Route path="/shipping" element={<Shipping />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/payments" element={<Payments />} />
+          <Route path="/shipping" element={<Shipping />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   );
 };
 
