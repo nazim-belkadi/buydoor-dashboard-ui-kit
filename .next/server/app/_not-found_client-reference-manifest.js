7c2d6361800be81d000fc99b038dd85152eb5236
nazim-belkadi <belkadinazim900@gmail.com>
2025-05-23 15:42:14 +0100

    Updated app-build-manifest.json

---
+++ .next/server/app/_not-found_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found"]={"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/home/project/src/app/not-found":[]}}
---
+++ .next/server/app/not-found_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/not-found"]={"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/home/project/src/app/not-found":[]}}
